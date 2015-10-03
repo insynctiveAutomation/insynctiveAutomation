e9ea5c10726c7af5e1fdc1e79ec42b7209c38a64
Eugenio Valeiras <Eugenio.Valeiras@gmail.com>
2015-10-03 18:36:03 -0300

    more time for CORS

--- src/main/java/insynctive/configuration/SimpleCORSFilter.java
+++ src/main/java/insynctive/configuration/SimpleCORSFilter.java
@@ -21,7 +21,7 @@ public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
     HttpServletResponse response = (HttpServletResponse) res;
     response.setHeader("Access-Control-Allow-Origin", "*");
     response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-    response.setHeader("Access-Control-Max-Age", "3600");
+    response.setHeader("Access-Control-Max-Age", "86400");
     response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     chain.doFilter(req, res);
 }
