b8979a2fe11ca01de6f3fe61767346b381df3e64
Eugenio Valeiras <Eugenio.Valeiras@gmail.com>
2015-10-20 13:57:13 -0300

    update unit.sql

--- init.sql
+++ init.sql
@@ -1,33 +1,38 @@
 /**CROSS BROWSER TESTING DATA**/
-INSERT INTO CrossBrowserAccount VALUES(1, "eugenio.valeiras+17@gmail.com", "u5d607b6a5c18b13");
+INSERT INTO CrossBrowserAccount VALUES(1, "eugenio.valeiras@gmail.com", "u88cf691bb46b53d");
+INSERT INTO CrossBrowserAccount VALUES(2, "rgonzalez@insynctive.com", "u89bf8bdf84b358d");
 
 /**US Address**/
 INSERT INTO usaddress VALUES(1, 1423, "City Bell", "Orange", true, "Optional Street", "short description", "California", "5th Street", 12345);
 INSERT INTO usaddress VALUES(2, 1423, "City Bell", "Orange", true, "Optional Street", "short description", "California", "5th Street", 12345);
 INSERT INTO usaddress VALUES(3, 1423, "City Bell", "Orange", true, "Optional Street", "short description", "California", "5th Street", 12345);
 INSERT INTO usaddress VALUES(4, 1423, "City Bell", "Orange", true, "Optional Street", "short description", "California", "5th Street", 12345);
+INSERT INTO usaddress VALUES(5, 1423, "City Bell", "Orange", true, "Optional Street", "short description", "California", "5th Street", 12345);
 
 /**Emergency Conctact**/
 INSERT INTO emergencycontact VALUES(1, "bpetrovski@insynctive.com", "Bojan Petrovski", 38922461333, "colleague");
 INSERT INTO emergencycontact VALUES(2, "bpetrovski@insynctive.com", "Bojan Petrovski", 38922461333, "colleague");
 INSERT INTO emergencycontact VALUES(3, "bpetrovski@insynctive.com", "Bojan Petrovski", 38922461333, "colleague");
 INSERT INTO emergencycontact VALUES(4, "bpetrovski@insynctive.com", "Bojan Petrovski", 38922461333, "colleague");
+INSERT INTO emergencycontact VALUES(5, "bpetrovski@insynctive.com", "Bojan Petrovski", 38922461333, "colleague");
 
 /**Person**/
 INSERT INTO person VALUES(1, "01/05/1970", "QA Department", "InsynctiveTestNG@gmail.com", 1, "Test", "Browser", 1, "CrossBrowsing", "Automated", "2015555554", "111223333", "QA", 1, 1);
 INSERT INTO person VALUES(2, "01/05/1970", "QA Department", "InsynctiveTestNG@gmail.com", 1, "Test", "Browser", 1, "CrossBrowsing", "Automated", "2015555554", "111223333", "QA", 2, 2);
 INSERT INTO person VALUES(3, "01/05/1970", "QA Department", "InsynctiveTestNG@gmail.com", 1, "Test", "Browser", 1, "CrossBrowsing", "Automated", "2015555554", "111223333", "QA", 3, 3);
 INSERT INTO person VALUES(4, "01/05/1970", "QA Department", "InsynctiveTestNG@gmail.com", 1, "Test", "Browser", 1, "CrossBrowsing", "Automated", "2015555554", "111223333", "QA", 4, 4);
+INSERT INTO person VALUES(5, "01/05/1970", "QA Department", "InsynctiveTestNG@gmail.com", 1, "Test", "Browser", 1, "CrossBrowsing", "Automated", "2015555554", "111223333", "QA", 5, 5);
 
 /**Insynctive Properties**/
 INSERT INTO insynctiveproperty VALUES(1,"staging","benefits123","password","evaleiras@insynctive.com", false, false);
 INSERT INTO insynctiveproperty VALUES(2,"staging","benefits123","password","evaleiras@insynctive.com", false, false);
 INSERT INTO insynctiveproperty VALUES(3,"staging","benefits123","password","evaleiras@insynctive.com", false, false);
 INSERT INTO insynctiveproperty VALUES(4,"staging","benefits123","password","evaleiras@insynctive.com", false, false);
+INSERT INTO insynctiveproperty VALUES(5,"staging","benefits123","password","evaleiras@insynctive.com", false, false);
 
 /**Accounts **/
-INSERT INTO account VALUES(1, "password",1, "EugeValeiras", 1, 1);
-INSERT INTO account VALUES(2, "password",3000, "FirstStep", 2, 1);
-INSERT INTO account VALUES(3, "password",1, "Bojan", 2, 2);
-INSERT INTO account VALUES(4, "password",1, "Cristi", 3, 3);
-INSERT INTO account VALUES(5, "password",1, "Rodrigo", 4, 4);
+INSERT INTO account VALUES(1, "password",265, "EugeValeiras", 1, 1);
+INSERT INTO account VALUES(2, "password",5000, "FirstStep", 2, 2);
+INSERT INTO account VALUES(3, "password",265, "Bojan", 3, 3);
+INSERT INTO account VALUES(4, "password",265, "Cristi", 4, 4);
+INSERT INTO account VALUES(5, "password",265, "Rodrigo", 5, 5);
